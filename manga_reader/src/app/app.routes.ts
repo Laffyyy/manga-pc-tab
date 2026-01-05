9177c12e62577a2cd97adc0b9d683dff549858a4
Laffyyy <95970405+Laffyyy@users.noreply.github.com>
2026-01-05 19:40:08 +0800

    added login and auth template

--- manga_reader/src/app/app.routes.ts
+++ manga_reader/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { AuthLayout } from './pages/auth-layout/auth-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        component: AuthLayout,
+        children: [
+            {
+                path: 'login',
+                component: Login
+            }
+        ],
+    }
+];
